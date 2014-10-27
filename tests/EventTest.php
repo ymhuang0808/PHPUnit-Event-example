b2565a44ae9368607c426a3c9b84009b01154eb5
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2014-10-27 23:38:40 +0800

    change identation

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -6,13 +6,13 @@ class EvnetTest extends \PHPUnit_Framework_TestCase
 	{
 		$event = new \PHPUnitEvent\Event();
 		$eventArray1 = array(
-							[
-								'name' => 'PHPUnit', 
-								'start_date' => '2014-10-27 09:00', 
-								'end_date' => '2014-10-27 12:00',
-								'description' => 'PHPUnit 教學 XD',
-							]
-						);
+			[
+				'name' => 'PHPUnit', 
+				'start_date' => '2014-10-27 09:00', 
+				'end_date' => '2014-10-27 12:00',
+				'description' => 'PHPUnit 教學 XD',
+			]
+		);
 		$event->setEvents($eventArray1);
 		$this->assertCount(1, $event->getEvents());
 	}
