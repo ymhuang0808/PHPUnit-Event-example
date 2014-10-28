2bee3f4b7b48434889f58c26fa344f7e79c84c85
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2014-10-28 10:01:58 +0800

    Add test events

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -5,18 +5,34 @@ class EvnetTest extends \PHPUnit_Framework_TestCase
     public function testSetAndGetEvent()
     {
         $event = new \PHPUnitEvent\Event();
-        $eventArray1 = array(
+        $eventArray = [
             [
                 'name' => 'PHPUnit', 
                 'start_date' => '2014-10-27 09:00', 
                 'end_date' => '2014-10-27 12:00',
                 'description' => 'PHPUnit 教學 XD',
+            ],
+            [
+                'name' => 'Learning Ruby on Rails',
+                'start_date' => '2014-11-02 09:30',
+                'end_date' => '2014-11-02 16:00',
+                'description' => 'RoR basic',
+            ],
+            [
+                'name' => 'Git tutorial',
+                'start_date' => '2014-11-08 14:00',
+                'end_date' => '2014-11-08 17:00',
+                'description' => 'How to use Git in your development?',
             ]
-        );
-        $event->setEvents($eventArray1);
-        $this->assertCount(1, $event->getEvents());
+        ];
+
+        $event->setEvents($eventArray);
+        $this->assertCount(3, $event->getEvents());
     }
 
+    /**
+     * @depends testSetAndGetEvent
+     */
     public function testGetValidEvents()
     {
         $event = new \PHPUnitEvent\Event();
