ef1f1e8e2839acee34e7b0fd276c7ac9f7ec7eec
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2014-10-28 20:14:05 +0800

    Not fix the data provider and dependencies problem

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -1,42 +1,54 @@
 <?php
 
-class EvnetTest extends \PHPUnit_Framework_TestCase
+class EventTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSetAndGetEvents()
+    public function eventsProvider()
     {
-        $event = new \PHPUnitEvent\Event();
-        $eventArray = [
+        return [
             [
-                'name' => 'PHPUnit', 
-                'start_date' => '2014-10-27 09:00', 
-                'end_date' => '2014-10-27 12:00',
-                'description' => 'PHPUnit tutorial XD',
-            ],
-            [
-                'name' => 'Learning Ruby on Rails',
-                'start_date' => '2014-11-02 09:30',
-                'end_date' => '2014-11-02 16:00',
-                'description' => 'RoR basic',
-            ],
-            [
-                'name' => 'Git tutorial',
-                'start_date' => '2014-11-08 14:00',
-                'end_date' => '2014-11-08 17:00',
-                'description' => 'How to use Git in your development?',
+                [
+                    [
+                        'name' => 'PHPUnit', 
+                        'start_date' => '2014-10-27 09:00', 
+                        'end_date' => '2014-10-27 12:00',
+                        'description' => 'PHPUnit tutorial XD',
+                    ],
+                    [
+                        'name' => 'Learning Ruby on Rails',
+                        'start_date' => '2014-11-02 09:30',
+                        'end_date' => '2014-11-02 16:00',
+                        'description' => 'RoR basic',
+                    ],
+                    [
+                        'name' => 'Git tutorial',
+                        'start_date' => '2014-11-08 14:00',
+                        'end_date' => '2014-11-08 17:00',
+                        'description' => 'How to use Git in your development?',
+                    ],
+                ]
             ]
         ];
+    }
 
+    /**
+     * @dataProvider eventsProvider
+     */
+    public function testSetAndGetEvents($eventArray)
+    {
+        $event = new \PHPUnitEvent\Event();
         $event->setEvents($eventArray);
-        $this->assertCount(, $event->getEvents());
+        $this->assertCount(3, $event->getEvents());
 
         return $event;
     }
 
     /**
-     * @depends testSetAndGetEvents 
+     * @depends testSetAndGetEvents
      */
     public function testGetValidEvents($event)
     {
+        var_dump($event);
+
         $expectedValidEvent = [
             [
                 'name' => 'Learning Ruby on Rails',
